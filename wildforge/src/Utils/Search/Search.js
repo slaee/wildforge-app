fe9b1bb35e4c1057be453326f56b6774c1eaf7f0
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-30 01:58:56 +0800

    Add Search Component

---
+++ wildforge/src/Utils/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Search.scss'
+
+function Search() {
+  return (
+    <div className='search-bar'>
+        <h2>Search</h2>
+        <input type="text" placeholder="Enter class info"/>
+    </div>
+  )
+}
+
+export default Search
