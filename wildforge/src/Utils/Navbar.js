7747e7a2057d85d89386449967d07c6754804297
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-30 00:32:27 +0800

    Update Navbar

--- wildforge/src/Utils/Navbar.js
+++ wildforge/src/Utils/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Styles/Navbar.scss'
 
 function Navbar({ buttons }) {
+  const [selectedButton, setSelectedButton] = useState(null)
+
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
+  
   return (
     <div className='parent'>
         <div className='title'>Class</div>
         {buttons.map(button => (
-          <div className={`${button.className}`} key={button.id}>
-        {button.label}
-      </div>
-    ))}
+        <div
+          className={`${button.className} ${selectedButton === button.id ? 'selected' : ''}`}
+          key={button.id}
+          onClick={() => handleButtonClick(button.id)}
+        >
+          {button.label}
+        </div>
+      ))}
     </div>
   )
 }
