3ad58608b7e4f7d5deba56a5224d46b1c8055859
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-09-24 14:15:51 +0800

    Update Login UI

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,35 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-import './Login.scss'
+import './Login.scss';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    // Add your login logic here using the 'email' and 'password' state variables
+    // For example, you can make an API request to authenticate the user.
+  };
+
   return (
     <>
-      <div className='app-name'>
-        <span class="wild fs-3 fw-bold">Wild</span><span class="fs-3 fw-bolder">FORGE</span>
+      <div className="app-name">
+        <span className="wild fs-3 fw-bold">Wild</span>
+        <span className="fs-3 fw-bolder">FORGE</span>
       </div>
-      <div className='login-container'>
-      <div className='login-form'>
-        <span class="fs-3 fw-bold pt-3 pb-3">Login</span>
-        <div class="d-flex flex-column">
-          <span class="fs-5 fw-bold pt-2 pb-2">Email</span>
-          <InputText class="yellow-on-focus" type="text"/>
+      <div className="login-container">
+        <div className="login-form">
+          <span className="fs-3 fw-bold pt-3 pb-3">Login</span>
+          <div className="d-flex flex-column">
+            <span className="fs-5 fw-bold pt-2 pb-2">Email</span>
+            <InputText
+              className="yellow-on-focus"
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+            />
+          </div>
+          <div className="d-flex flex-column pt-3 pb-3">
+            <span className="fs-5 fw-bold pt-2 pb-2">Password</span>
+            <InputText
+              className="yellow-on-focus"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <a href="/" className="d-flex justify-content-end fs-5 redirect-text">
+              Forgot Password
+            </a>
+          </div>
+          <div className="d-grid gap-2 pt-3 pb-3">
+            <button
+              type="button"
+              className="btn btn-wild-primary btn-large fw-bold fs-5"
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </div>
+          <div className="d-flex justify-content-center pt-3 pb-3">
+            <span className="fs-5">
+              Don't have an account? <a href="/" className="redirect-text">Sign Up</a>
+            </span>
+          </div>
         </div>
-        <div class="d-flex flex-column pt-3 pb-3">
-          <span class="fs-5 fw-bold pt-2 pb-2">Password</span>
-          <InputText class="yellow-on-focus" type="text"/>
-          <a href="/" class="d-flex justify-content-end fs-5 redirect-text">Forgot Password</a>
-        </div>
-        <div class="d-grid gap-2 pt-3 pb-3">
-          <button type="button" class="btn btn-wild-primary btn-large fw-bold fs-5">Login</button>
-        </div>
-        <div class="d-flex justify-content-center pt-3 pb-3">
-          <span class="fs-5">Don't have an account? <a href="/" class="redirect-text">Sign Up</a></span>
-        </div>
-      </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
