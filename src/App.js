414f6e53c0feb1b431b7291059befcc84b68a9dd
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-09-24 14:16:23 +0800

    Update Global CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './screens/Login/Login';
+import Signup from './screens/Signup/Signup';
 
 function App() {
   return (
     <>
-    <Login/>
+    
+    <Signup/>
     </>
   );
 }
