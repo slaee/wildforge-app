f4f8f097cceef6852b10fbbf50033b927615b1c4
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-09-29 17:37:57 +0800

    Add Cards Component

--- src/components/cards/WildCards.js
+++ src/components/cards/WildCards.js
@@ -3,8 +3,12 @@ import './index.scss'
 
 function WildCards() {
   return (
-    <div className='class-card d-flex justify-content-center align-content-center'>
-      <span className='fw-bold'>Class Name</span>
+    <div className='class-card d-flex flex-column justify-content-center p-5'>
+      <span className='text-center fw-bold fs-5 my-1'>Class Name</span>
+      <div className='d-flex flex-row justify-content-center my-1'>
+        <span className='text-center fw-semibold mx-1'>Section</span>
+        <span className='text-center fw-semibold mx-1'>Schedule</span>
+      </div>
     </div>
   )
 }
