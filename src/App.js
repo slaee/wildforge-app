f919a0fde7936a2f7f4cded6457c136c43eaa92a
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-09-29 17:44:32 +0800

    Update Login Route

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
         <Route path="/forgotpassword" element={<ForgotPassword/>}/>
         <Route path="/dashboard" element={<ViewClass/>}>
