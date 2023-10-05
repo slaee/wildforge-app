ebc9461ed9419b4418623542e0847d437c92caae
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-06 00:23:39 +0800

    Add useCreateClass

---
+++ src/hooks/useCreateClass.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+import { ClassesService } from '../services';
+
+const useCreateClass = () => {
+  const [isCreating, setIsCreating] = useState(false);
+
+  const createClass = async ({ name, sections, schedule, callbacks }) => {
+    setIsCreating(true);
+
+    let responseCode;
+    let retrievedClass;
+
+    try {
+      const { status, data } = await ClassesService.create({
+        name,
+        sections,
+        schedule,
+      });
+
+      responseCode = status;
+      retrievedClass = data;
+    } catch (error) {
+      responseCode = error.response.status;
+    }
+
+    switch (responseCode) {
+      case 201:
+        await callbacks.created({ retrievedClass });
+        break;
+      case 400:
+        await callbacks.invalidFields();
+        break;
+      case 500:
+        await callbacks.internalError();
+        break;
+      default:
+    }
+
+    setIsCreating(false);
+  };
+
+  return { isCreating, createClass };
+};
+
+export default useCreateClass;
