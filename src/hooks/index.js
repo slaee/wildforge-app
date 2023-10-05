946b72bc348a884cc38a241911dfc5179d117b6b
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-05 22:55:42 +0800

    Register useClasses

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useLogin } from './useLogin';
 export { default as useSignup } from './useSignup';
 export { default as useAcquireTokens } from './useAcquireTokens';
+export { default as useClasses } from './useClasses';
