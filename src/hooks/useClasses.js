49b1d2a049aefaa8c1a596b0ca89086882f92b9f
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-05 22:19:41 +0800

    Add useClasses

---
+++ src/hooks/useClasses.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ClassesService } from '../services';
+
+const useClasses = () => {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(true);
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    const listRooms = async () => {
+      let responseCode;
+      let retrievedClasses;
+
+      try {
+        const { status, data } = await ClassesService.list();
+
+        responseCode = status;
+        retrievedClasses = data;
+      } catch (error) {
+        responseCode = error.response.status;
+      }
+
+      switch (responseCode) {
+        case 200:
+          setClasses(retrievedClasses);
+          break;
+        case 401:
+        case 404:
+        case 500:
+          navigate('/logout');
+          break;
+        default:
+      }
+
+      setIsLoading(false);
+    };
+
+    listRooms();
+  }, []);
+
+  return { isLoading, classes };
+};
+
+export default useClasses;
