b5f4fd2b7ed148eca191bb28bfed54d6b40d73ef
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-06 19:26:06 +0800

    Update host and port configurations

--- src/services/config.js
+++ src/services/config.js
@@ -1,9 +1,12 @@
 import { isLocal } from '../utils/destination';
 
 let apiUrl = null;
+const apiHost = process.env.BACKEND_CONTAINER_HOST ?? '127.0.0.1';
+const apiPort = process.env.BACKEND_CONTAINER_PORT ?? '8000';
 
 if (isLocal) {
-  apiUrl = 'http://0.0.0.0:8000';
+  // Development
+  apiUrl = `http://${apiHost}:${apiPort}`;
 } else {
   // Production
   apiUrl = 'https://api.our.link';
